744f920362a8e3accfa98f0420ec080a107f1e89
joshuapawlik <joshawesome12@yahoo.com>
2018-07-10 23:42:06 -0500

    Adds Example Image

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -16,7 +16,7 @@ export default class Menu extends Component {
       <h1 className="menu-title">This is the menu Component</h1>
       <div className='menu-items'>
         <h2>Item</h2>
-        <img src='' alt=''/>
+        <img src='../Resources/sno-cones-background.gif' alt='snow cone'/>
         <h2>Item</h2>
         <img src='' alt=''/>
         <h2>Item</h2>
